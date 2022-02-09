84442423718cf7c194ad442c14583665f3d040be
ricobl4ck <adrishahri@gmail.com>
2022-02-09 14:56:31 +0800

    feat: AVRO schema implementation

--- consumer.js
+++ consumer.js
@@ -1,18 +1,20 @@
 const { Kafka } = require('kafkajs')
 const { v4 } = require('uuid')
 require("dotenv").config()
+const { SchemaRegistry } = require('@kafkajs/confluent-schema-registry')
+
 
 const kafka = new Kafka({
   clientId: 'AppName',
-  brokers: ['0.0.0.0:9092'],
-  // brokers: ["process.env.BROKER_URL1","process.env.BROKER_URL2","process.env.BROKER_URL3"]
-
+  // brokers: ['0.0.0.0:9092'],
+  brokers: [process.env.BROKER_URL1, process.env.BROKER_URL2, process.env.BROKER_URL3]
 })
 
 
 async function sub() {
 
-  const consumer = kafka.consumer({ groupId: 'AppName: ' + v4() })
+  const registry = new SchemaRegistry({ host: 'http://localhost:8081/' })
+  const consumer = kafka.consumer({ groupId: 'Consumer: ' + v4() })
 
   await consumer.connect()
 
@@ -21,33 +23,24 @@ async function sub() {
   // consumer.on(HEARTBEAT, e =>
   //   console.log(`heartbeat ${e.id} at ${e.timestamp} type ${e.type} inside group ${e.payload.groupId}`))
 
-  const { REQUEST } = consumer.events
-  consumer.on(REQUEST, e =>
-    console.log(`REQUEST ID ${e.id} at ${e.timestamp} TYPE ${e.type}  
-    PAYLOAD ${e.payload.broker} ${e.payload.clientId}, ${e.payload.createdAt}, ${e.payload.sentAt}`))
+  // const { REQUEST } = consumer.events
+  // consumer.on(REQUEST, e =>
+  //   console.log(`REQUEST ID ${e.id} at ${e.timestamp} TYPE ${e.type}  
+  //   PAYLOAD ${e.payload.broker} ${e.payload.clientId}, ${e.payload.createdAt}, ${e.payload.sentAt}`))
 
   // {broker, clientId, correlationId, size, createdAt, sentAt, pendingDuration, duration, apiName, apiKey, apiVersion}
 
 
-  await consumer.subscribe({ topic: 'EPL', fromBeginning: true })
-
-  /**
-   * Normal consume - eachMessage
-   * */
+  await consumer.subscribe({ topic: 'test-schema', fromBeginning: true })
 
   let msgCount = 0
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       msgCount++
-      console.log({
-        topic: topic,
-        key: message.key.toString(),
-        value: message.value.toString(),
-        offset: message.offset,
-        count: msgCount
-        // partition: partition,
-      })
+      const decodedValue = await registry.decode(message.value)
+      // console.log({ topic: topic, offset: message.offset, count: msgCount })
+      console.log(decodedValue)
     },
   })
 
--- producer.js
+++ producer.js
@@ -1,42 +1,49 @@
 const { Kafka } = require('kafkajs')
 require("dotenv").config()
+const { SchemaRegistry, SchemaType } = require('@kafkajs/confluent-schema-registry')
+
 
 
 const kafka = new Kafka({
   clientId: 'AppName',
-  brokers: ['0.0.0.0:9092'],
-  // brokers: ["process.env.BROKER_URL1","process.env.BROKER_URL2","process.env.BROKER_URL3"]
+  // brokers: ['0.0.0.0:9092'],
+  brokers: [process.env.BROKER_URL1,process.env.BROKER_URL2,process.env.BROKER_URL3]
 
 })
 
-
 async function pub() {
+
+  const schema = {
+    name: "animalist",
+    namespace: "binatang",
+    type: "record",
+    fields: [
+      { name: "name", type: "string" },
+      { name: "breed", type: "string" },
+      { name: "color", type: "string" },
+    ],
+  };
+
+  const registry = new SchemaRegistry({ host: 'http://localhost:8081' })
+
+  const { id } = await registry.register({
+    type: SchemaType.AVRO,
+    schema: JSON.stringify(schema),
+  });
 
 
   const producer = kafka.producer()
 
   await producer.connect()
 
+  // Encode using the uploaded schema
+  const payload = { name: "Charlie", breed: "Golden Retrievers", color: "Gold" };
+  const encodedValue = await registry.encode(id, payload);
 
-  const topicMessages = [
-    {
-      topic: 'EPL',
-      messages: [
-        {
-          key: 'key1',
-          value: 'AppName1',
-          // partition: 0
-        },
-        {
-          key: 'key2',
-          value: 'AppName2',
-          // partition: 1
-        }
-      ],
-    }]
-
-
-  await producer.sendBatch({topicMessages})
+  await producer.send({
+    topic: 'test-schema',
+    messages: [{ value: encodedValue }],
+  })
 
   await producer.disconnect()
 }
